8a10a89d274d6deaef14c3e78df88add9f58a2a5
andela-batolagbe <atolagbe.bisoye@andela.co>
2015-08-03 12:24:40 +0100

    #6, include e2e test to check if server is up

---
+++ app/test/server-spec.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var request = require('supertest');
+var express = require('express');
+var app = require('../../server.js');
+
+describe("server test", function() {
+
+  it('should respond on connetion /',
+  function(done) {
+
+    request(app)
+      .get('/')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done()
+      });
+
+  });
+
+});
