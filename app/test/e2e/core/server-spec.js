3777864332dd6ca2cef34804011d1385620bd3d1
andela-batolagbe <atolagbe.bisoye@andela.co>
2015-08-05 12:43:47 +0100

    test:<server>: restructure the test directory
    1. Include subfolders `unit`, `user`, `e2e`, and `core`
    2. re-indent codes in the `it` block
    3. remove uneccesary `.js`

--- app/test/e2e/core/server-spec.js
+++ app/test/e2e/core/server-spec.js
@@ -2,19 +2,19 @@
 
 var request = require('supertest');
 var express = require('express');
-var app = require('../../server.js');
+var app = require('../../../../server');
 
 describe("server test", function() {
 
   it('should respond on connection /api',
     function(done) {
 
-      request(app)
-        .get('/api')
-        .expect(200)
-        .end(function(err, res) {
-          if (err) return done(err);
-          done();
-        });
+    request(app)
+    .get('/api')
+    .expect(200)
+    .end(function(err, res) {
+      if (err) return done(err);
+      done();
+      });
     });
 });
