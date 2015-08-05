3f115e2c9632b411a7999ab9601eae1cc02b84d5
andela-batolagbe <atolagbe.bisoye@andela.co>
2015-08-05 14:28:24 +0100

    Fix(server): add the request variable
    add the request variable initially removed

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(res) {
+router.get('/', function(req, res) {
   res.sendFile('index.html', {'root': './'});
 });
 
