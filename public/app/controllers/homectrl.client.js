8144a8cfd0a506ff362783ef47bdb9a842bd84ad
Susan <susan.adelokiki@andela.co>
2015-08-12 10:16:55 +0100

    Create controllers for the views.

---
+++ public/app/controllers/homectrl.client.js
@@ -0,0 +1,26 @@
+"use strict"
+
+app.controller('HomeCtrl', ['$scope', function($scope) {
+    $(function() {
+        // OPACITY OF BUTTON SET TO 0%
+        $(".roll").css("opacity", "0");
+
+        // ON MOUSE OVER
+        $(".roll").hover(function() {
+
+                // SET OPACITY TO 70%
+                $(this).stop().animate({
+                    opacity: .7
+                }, "slow");
+            },
+
+            // ON MOUSE OUT
+            function() {
+
+                // SET OPACITY BACK TO 50%
+                $(this).stop().animate({
+                    opacity: 0
+                }, "slow");
+            });
+    });
+}]);
