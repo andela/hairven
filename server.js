adaac5df9408c9cbd24a52460f007e4fa14f402e
andela-batolagbe <atolagbe.bisoye@andela.co>
2015-08-21 10:32:03 +0100

    Refactor(Server): Refactor server to load public/index

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+<<<<<<< HEAD
 'use strict';
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var app = require('./config/express');
 var port = process.env.PORT || 5000;
+=======
+"use strict";
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = require('./config/express');
+var port = process.env.PORT || 5000;
+
+>>>>>>> Refactor(Server): Refactor server to load public/index
 
 app.listen(port);
+
 console.log('now listening on port: ' + port);
 
 module.exports = app;
