5247da86ae9f1d522b17e4b7a937ebec697219d2
Abdulmalik <abdulmalik.yusuf@andela.com>
2015-10-01 18:28:10 +0100

    feat(backend): add twitter auth strategy

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 var jwt = require('jsonwebtoken');
 var User = require('../models/user.model');
-var db = require('../../config/config');
+var config = require('../../config/config');
 var passport = require('passport');
 
 exports.signup = function(req, res) {
@@ -13,13 +13,13 @@ exports.signup = function(req, res) {
     if (!user.username||!user.email||!user.password) {
       return res.status(401).send({
         success: false,
-        message: 'Invalid Username/Email/Password.'});
+        message: 'Invalid Username or Email or Password!'});
     }
     else if (err) {
       if (err.code === 11000) {
         return res.status(401).send({
           success: false,
-          message: 'User already exists.'});
+          message: 'Username already exists!'});
       }
       else {
           return res.status(401).send(err);
@@ -40,15 +40,39 @@ exports.facebookLogin = function(strategy) {
       else if (!user) {
         return res.status(401).send({ 
         success: false,
-        message: 'Authentication failed. User not found.' });
+        message: 'Invalid Username or Password!' });
       }
       else {
-        var token = jwt.sign(user, db.secret, {
+        var token = jwt.sign(user, config.secret, {
           expiresInMinutes: 1440
         });
         res.json({
-        success: true,
-        message: 'Welcome to Hairven'
+          success: true,
+          message: 'Welcome to Hairven'
+        });
+      }
+    })(req, res, next);
+  };
+};
+
+exports.twitterLogin = function(strategy) {
+  return function(req, res, next) {
+    passport.authenticate(strategy, function(err, user) {
+      if (err) {
+        throw (err);
+      }
+      else if (!user) {
+        return res.status(401).send({ 
+        success: false,
+        message: 'Invalid Username or Password!' });
+      }
+      else {
+        var token = jwt.sign(user, config.secret, {
+          expiresInMinutes: 1440 
+        });
+        res.json({
+          success: true,
+          message: 'Welcome to Hairven'
         });
       }
     })(req, res, next);
@@ -66,17 +90,17 @@ exports.login = function(req, res) {
     if (!user) {
       return res.status(401).send({
         success: false,
-        message: 'Authentication failed. User not found.' });
+        message: 'Invalid Username or Password!' });
     }
     else {
       var validPassword = user.comparePassword(req.body.password);
       if (!validPassword) {
         return res.status(401).send({
           success: false,
-          message: 'Authentication failed. Wrong password.' });
+          message: 'Invalid Username or Password!' });
       }
       else {
-        var token = jwt.sign(user, db.secret, {
+        var token = jwt.sign(user, config.secret, {
           expiresInMinutes: 1440
         });
         res.json({
@@ -86,55 +110,5 @@ exports.login = function(req, res) {
         });
       }
     }
-  });
-};
-
-exports.middleware = function(req, res, next) {
-  var token = req.body.token ||
-    req.query.token ||
-    req.headers['x-access-token'];
-  if (token) {
-    jwt.verify(token, db.secret, function(err, decoded) {
-      if (err) {
-        return res.json({ success: false,
-          message: 'Failed to authenticate token.' });
-      }
-      else {
-        req.decoded = decoded;
-        next();
-      }
-    });
-  }
-  else {
-    return res.status(403).send({
-        success: false,
-        message: 'No token provided.'
-    });
-  }
-};
-
-exports.getUser = function(req, res) {
-  User.find({username: req.params.username}, function(err, user) {
-    if (err) {
-      res.send(err);
-    }
-    res.json(user);
-    });
-};
-
-exports.editProfile = function(req, res) {
-  User.update({username: req.params.username},
-    req.body, function() {
-    res.json({ message: 'User updated!' });
-  });
-};
-
-exports.deleteUser = function(req, res) {
-  User.remove({username: req.params.username},
-    function(err) {
-    if (err) {
-      return res.send(err);
-    }
-    res.json({ message: 'Successfully deleted' });
   });
 };
