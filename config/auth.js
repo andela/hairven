b027b034d65ff5a531c4a9a49dec62e78859fced
Abdulmalik <abdulmalik.yusuf@andela.com>
2015-10-01 18:25:17 +0100

    feat(backend): add reference to facebook and twitter credentials

--- config/auth.js
+++ config/auth.js
@@ -4,6 +4,12 @@ module.exports = {
     'clientID': process.env.FACEBOOK_CLIENT_ID,
     'clientSecret': process.env.FACEBOOK_CLIENT_SECRET,
     'callbackURL': process.env.FACEBOOK_CALLBACK_URL
+  },
+
+  'twitterAuth' : {
+    'consumerKey': process.env.TWITTER_CONSUMER_KEY,
+    'consumerSecret': process.env.TWITTER_CONSUMER_SECRET,
+    'callbackURL': process.env.TWITTER_CALLBACK_URL
   }
 
 };
