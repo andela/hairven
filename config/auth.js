49aa2c313123ab4a123f9d1bf56a982444c4fe18
andela-batolagbe <atolagbe.bisoye@andela.co>
2015-10-20 13:43:33 +0100

    style(auth): remove white spaces

--- config/auth.js
+++ config/auth.js
@@ -3,13 +3,13 @@ module.exports = {
   facebookAuth: {
     clientID: '1863446777214443',
     clientSecret: '9d077e90655b6cef098b8e779d1afeca',
-    callbackURL: 'http://localhost:5000/auth/facebook/callback'
+    callbackURL: 'http://localhost:3030/auth/facebook/callback'
   },
 
   twitterAuth: {
     consumerKey: 'rX5BNnCcFEsyoP2WTnjg98MP2',
     consumerSecret: 'QzuIMMHFT9aeI4wIKRda2HbPmMJ0SZrWNjQMQrrqhFVkLZSIge',
-    callbackURL: 'http://localhost:5000/auth/twitter/callback'
+    callbackURL: 'http://localhost:3030/auth/twitter/callback'
   }
 
 
