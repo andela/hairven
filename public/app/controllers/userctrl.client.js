eb1b0bcb891cdb8532ac2bac027f25a1c4d7f67a
andela-batolagbe <atolagbe.bisoye@andela.co>
2015-10-20 13:08:39 +0100

    Feat(client userCntrl): add social media methods
    add scope methods for facebook and twitter
    redefine login and logout scope
    add satellizer token destroy feature to logout

--- public/app/controllers/userctrl.client.js
+++ public/app/controllers/userctrl.client.js
@@ -1,9 +1,8 @@
 'use strict';
 
-console.log('User controller');
 angular.module('hairvenApp')
-  .controller('UserCtrl', ['UserService', '$rootScope', '$scope', '$location', '$localStorage',
-    function(UserService, $rootScope, $scope, $location, $localStorage) {
+  .controller('UserCtrl', ['UserService', '$rootScope', '$window', '$scope', '$location', '$localStorage', '$auth',
+    function(UserService, $rootScope, $window, $scope, $location, $localStorage, $auth) {
 
       function successAuth(res) {
         console.log(res);
@@ -11,24 +10,51 @@ angular.module('hairvenApp')
           console.log('stop', data);
         } else {
           console.log(res.body);
-          $localStorage.token = res.data.token
-          console.log()
+          $localStorage.token = res.data.token;
+
           alert('You are in!');
         }
       };
 
+      $rootScope.facebookSignin = function() {
+        UserService.facebookLogin(function(res) {
+          if (res.type === false) {
+            console.log('error loggin in');
+          } else {
+
+          }
+
+        });
+
+      };
+
+      $rootScope.twitterSignin = function() {
+
+        UserService.twitterLogin(function(res) {
+          if (res.status === 200) {
+            $location.path('https://api.twitter.com/oauth/authenticate?oauth_token=_nQU2gAAAAAAg6_NAAABUIEdzWg');
+          } else {
+
+          }
+
+        });
+
+
+      };
       $rootScope.signin = function() {
+
         var data = {
           username: $scope.username,
           password: $scope.password
         };
         UserService.login(data, function(res) {
-        console.log('response to login', res);
+          $auth.setToken();
           if (res.type === false) {
-            console.log('authentication failed', data)
+            console.log('authentication failed', data);
           } else {
-            $localStorage.token = res.data.token;
-            console.log('you are signed in')
+            console.log(res.data.token);
+            $localStorage.token = res.token;
+            console.log('you are signed in');
           }
         }, function(err) {
           console.log('login failed', err);
@@ -44,26 +70,47 @@ angular.module('hairvenApp')
         };
         UserService.register(data, function(data) {
           if (data.success === false) {
-            console.log('registration failed')
-            $location.path('/')
+            console.log('registration failed');
+            $location.path('/');
           } else {
             // $localStorage.token = data.token;
             console.log('you are registered');
           }
         }, function(err) {
           console.log('FAILED!!', err);
-          $rootScope.error = 'Failed to sign up'
+          $rootScope.error = 'Failed to sign up';
         });
       };
 
       $scope.currentUser = UserService.currentUser();
       $rootScope.logout = function() {
+        $auth.removeToken();
         UserService.logout(function() {
           window.location = '/';
         }, function() {
-          console.log('failed to logout!')
+          console.log('failed to logout!');
         });
       };
       $scope.token = $localStorage.token;
+
+      $rootScope.authenticate = function(provider) {
+        $auth.authenticate(provider)
+          .then(function(response) {
+
+            $window.localStorage.currentUser = JSON.stringify(response.access_token);
+            $rootScope.currentUser = JSON.parse($window.localStorage.currentUser);
+          })
+          .catch(function(response) {
+          });
+      };
+
+      $rootScope.isAuthenticated = function() {
+
+        $auth.isAuthenticated().then(function() {
+          return true;
+        }).catch(function() {
+          return false;
+        });
+      };
     }
   ]);
