e93469b870e10e8cf8e747e06622d3cad767335f
andela-batolagbe <atolagbe.bisoye@andela.co>
2015-11-04 17:30:00 +0100

    feat(test): add first and last name test validation test for model

--- spec/unit/user/user.model.spec.js
+++ spec/unit/user/user.model.spec.js
@@ -2,54 +2,98 @@ var User = require('../../../app/models/user.model');
 
 var user;
 
-describe('User Model', function(){
-  beforeEach(function(done){
+describe('User Model', function() {
+  beforeEach(function(done) {
     user = new User();
     done();
   });
 
-  it('should not register if username field is empty', function(done){
+  it('should not register if firstname field is empty', function(done) {
+    user.name = {
+      firstname: '',
+      lastname: 'David'
+    };
     user.username = '';
     user.email = 'badman@gmail.com';
     user.password = 'badbad';
-    user.save(function(err){
+    user.save(function(err) {
       expect(err).not.toBe(null);
       done();
     });
   });
 
-  it('should not register if email field is empty', function(done){
+  it('should not register if lastname field is empty', function(done) {
+    user.name = {
+      firstname: 'John',
+      lastname: ''
+    };
+    user.username = '';
+    user.email = 'badman@gmail.com';
+    user.password = 'badbad';
+    user.save(function(err) {
+      expect(err).not.toBe(null);
+      done();
+    });
+  });
+
+  it('should not register if username field is empty', function(done) {
+    user.name = {
+      firstname: 'John',
+      lastname: 'David'
+    };
+    user.username = '';
+    user.email = 'badman@gmail.com';
+    user.password = 'badbad';
+    user.save(function(err) {
+      expect(err).not.toBe(null);
+      done();
+    });
+  });
+
+  it('should not register if email field is empty', function(done) {
+    user.name = {
+      firstname: 'John',
+      lastname: 'David'
+    };
     user.username = 'badman';
     user.email = '';
     user.password = 'badbad';
-    user.save(function(err){
+    user.save(function(err) {
       expect(err).not.toBe(null);
       done();
     });
   });
 
-  it('should not register if password field is empty', function(done){
+  it('should not register if password field is empty', function(done) {
+    user.name = {
+      firstname: 'John',
+      lastname: 'David'
+    };
     user.username = 'badman';
     user.email = 'badman@gmail.com';
     user.password = '';
-    user.save(function(err){
+    user.save(function(err) {
       expect(err).not.toBe(null);
       done();
     });
   });
 
-  it('should register a user with complete credentials', function(done){
+  it('should register a user with complete credentials', function(done) {
+    user.name = {
+      firstname: 'John',
+      lastname: 'David'
+    };
     user.username = 'badman';
     user.email = 'badman@gmail.com';
     user.password = 'badbad';
-    user.save(function(err){
-      expect(err).toBe(null); 
+    user.save(function(err) {
+      expect(err).toBe(null);
       done();
     });
   });
 
   afterEach(function(done) {
-    User.remove({}, function () {
+    User.remove({}, function() {
       done();
     });
   });
