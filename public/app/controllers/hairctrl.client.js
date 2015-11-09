60675ba5ebac17ab367d25e5f24f3e09660a767a
andela-batolagbe <atolagbe.bisoye@andela.co>
2015-11-09 14:48:32 +0100

    Feat(client controllers): add hairstyle controller

--- public/app/controllers/hairctrl.client.js
+++ public/app/controllers/hairctrl.client.js
@@ -1,4 +1,76 @@
 "use strict"
 
 angular.module('hairvenApp')
-  .controller('HairCtrl', ['$scope', function($scope) {}]);
+  .controller('HairCtrl', ['HairstyleService', '$scope', '$state', '$rootScope', '$localStorage',
+    function($scope, HairstyleService, $state, $rootScope, $localStorage) {
+
+      $scope.addHairstyle = function(salonId) {
+
+        var data = {
+          name: $scope.name,
+          description: $scope.description,
+          salon: salonId
+        };
+
+        HairstyleService.addHairstyle(data).success(function(res) {
+          console.log(res.message);
+          $state.go('salongallery')
+        }).error(function(err) {
+
+          console.log(err);
+        });
+      };
+
+
+      $scope.getAllHairstyles = function() {
+
+        HairstyleService.getAllHairstyles().success(function(res) {
+          console.log(res.body);
+          $state.go('gallery')
+        }).error(function(err) {
+
+          console.log(err);
+        });
+      };
+
+      $scope.getOneHairstyle = function(hairId) {
+
+        HairstyleService.getOneHairstyle(hairId).success(function(res) {
+          $localStorage.currentHairstyle = res.body;
+        }).error(function(err) {
+
+          console.log(err);
+        });
+
+      };
+
+      $scope.updateHairstyle = function(hairId) {
+
+        var data = {
+          name: $scope.name,
+          description: $scope.description,
+          salon: salonId
+        };
+
+        HairstyleService.updateHairstyle(data).success(function(res) {
+          console.log(res.message);
+          $state.go('salongallery')
+        }).error(function(err) {
+
+          console.log(err);
+        });
+      };
+
+      $scope.deleteHairstyle = function(hairId) {
+
+        HairstyleService.deleteHairstyle(hairId).success(function(res) {
+          console.log(res.message);
+        }).error(function(err) {
+
+          console.log(err);
+        });
+
+      };
+
+    }
+  ]);
